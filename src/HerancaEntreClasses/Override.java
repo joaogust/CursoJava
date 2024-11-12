c125d83344c9a4a849ea13dbc7e0e7e0b3a48d5d
João Gustavo <joaogustavodossantoss@gmail.com>
2024-11-12 10:13:11 -0300

    Atualização de anotações na classe Override

--- src/HerancaEntreClasses/Override.java
+++ src/HerancaEntreClasses/Override.java
@@ -9,6 +9,9 @@ public class Override {
     - Ele faz com que você evite sobrescrever um metodo com parametros de tipos diferentes
     te avisando que o código não pode ser compilado. Sem ele o código rodaria com erros
     concequentes dessa ação incorreta;
+    - Ex: se na classe pai um método recebe o parâmetro int e na subclasse o método recebe um double
+    o compilador vai entender que seria um polimorfismo de sobrecarga e não de sobrescrita, causando, 
+    em alguns casos, erros de compilação. Se estiver escrito override isso não acontece pois ele te avisa.
     - Ele te ajuda a mostrar possiveis erros na hora de sobrescrever;
     - SEMPRE USE ELE!
      */
