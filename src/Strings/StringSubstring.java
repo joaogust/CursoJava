166baacafb95de4b8b740862e432c7733a1d1300
joaogust <joaogustavodossantoss@gmail.com>
2025-02-21 18:40:37 -0300

    Aula 162

---
+++ src/Strings/StringSubstring.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class StringSubstring {
+
+    public static void main(String[] args) {
+
+        String str = "Java is the greatest language!";
+
+        String substr1 = str.substring(4).trim();
+
+        System.out.println(substr1);
+
+
+        String substr2 = str.substring(4, str.length()-1);
+
+        System.out.println(substr2);
+
+        System.out.println(str);
+    }
+}
