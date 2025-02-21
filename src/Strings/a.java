8442010ba4f56afd4a1eb9b6bff9eb95a22a2d51
joaogust <joaogustavodossantoss@gmail.com>
2025-02-21 17:27:08 -0300

    Aula 156

---
+++ src/Strings/a.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class a {
+    public static void main(String[] args) {
+        String k = "v";
+        String j = k;
+        k = "9";
+
+        System.out.println(k);
+        System.out.println(j);
+    }
+}
