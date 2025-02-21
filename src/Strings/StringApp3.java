02a27033a2deb965523ffe46b381e484dcf76da5
joaogust <joaogustavodossantoss@gmail.com>
2025-02-21 18:29:23 -0300

    Aula 160

---
+++ src/Strings/StringApp3.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class StringApp3 {
+    public static void main(String[] args) {
+
+//        String s = "";
+//        for(char c = 'A'; c <= 'Z'; c++) {
+//            s += c;
+//        }
+//        System.out.println(s);
+
+        StringBuilder s = new StringBuilder();
+        for(char c = 'A'; c <= 'Z'; c++) {
+            s.append(c);
+        }
+
+        System.out.println(s);
+
+        String s2 = s.toString();
+        System.out.println();
+        System.out.println(s2);
+    }
+}
+/*
+    Utilizando o StringBuilder, espaços de memória são economizados e não acumulados como na 'img_2'.
+
+    Para concatenação dentro de loops, o StringBuilder deve ser utilizado.
+ */
