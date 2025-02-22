922dcf6778d9e439e96331feba3b61cf5a84fdde
joaogust <joaogustavodossantoss@gmail.com>
2025-02-22 14:11:36 -0300

    Aula 168

---
+++ src/Enums/EnumApp1.java
@@ -0,0 +1,15 @@
+package Enums;
+
+enum Month {
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+}
+
+public class EnumApp1 {
+
+    public static void main(String[] args) {
+
+        Month firstMonth = Month.JANUARY;
+        Month noMonth = null;
+//        Month testMonth = Month.SOMETHING;
+    }
+}
