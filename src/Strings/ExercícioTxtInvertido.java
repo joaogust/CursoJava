28f74f39f4b80491ee10bb49af5bf01bee9247e6
joaogust <joaogustavodossantoss@gmail.com>
2025-02-22 09:47:56 -0300

    Aula 166: Exercício

---
+++ src/Strings/ExercícioTxtInvertido.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class ExercícioTxtInvertido {
+
+    public static void main(String[] args) {
+
+        var scanner = new java.util.Scanner(System.in);
+
+        System.out.println("Digite o texto: ");
+
+        String txt = scanner.nextLine();
+
+        System.out.println(new StringBuilder(txt).reverse());
+
+    }
+}
