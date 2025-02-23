c1e45cc285a8fa4e039188221424ec82a7f3aade
joaogust <joaogustavodossantoss@gmail.com>
2025-02-23 17:07:28 -0300

    Aula 183

---
+++ src/ClassesWrappers/Exercícios/Ex1/Operation.java
@@ -0,0 +1,6 @@
+package ClassesWrappers.Exercícios.Ex1;
+
+public interface Operation {
+
+    Integer execute(Integer n1, Integer n2);
+}
---
+++ src/ClassesWrappers/Exercícios/Ex1/OperationImpl.java
@@ -0,0 +1,22 @@
+package ClassesWrappers.Exercícios.Ex1;
+
+public class OperationImpl implements Operation{
+
+    @Override
+    public Integer execute(Integer n1, Integer n2) {
+        n1 = n1 == null ? 0 : n1;
+        n2 = n2 == null ? 0 : n2;
+        return n1 + n2;
+    }
+
+    public static void main(String[] args) {
+
+        Operation n = new OperationImpl();
+
+        int h = n.execute(10, 40);
+        System.out.println(h);
+
+        h = n.execute(null, 40);
+        System.out.println(h);
+    }
+}
