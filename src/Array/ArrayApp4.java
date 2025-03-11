498d78b7463a9e28b6a46e6f01ce532bf74af52c
joaogust <joaogustavodossantoss@gmail.com>
2025-03-11 16:14:43 -0300

    Aula 219

---
+++ src/Array/ArrayApp4.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class ArrayApp4 {
+
+    public static void main(String[] args) {
+
+        int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19};
+
+
+        for (int i = 0; i < primeNumbers.length; i++) {
+            System.out.printf("%d -> %d\n", i, primeNumbers[i]);
+        }
+
+        for (int i: primeNumbers) {
+            System.out.printf("-> %d\n", i);
+        }
+    }
+}
