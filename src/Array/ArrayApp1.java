0fcb933f9e4289c4950a29e566b48f1764592dc5
joaogust <joaogustavodossantoss@gmail.com>
2025-03-11 15:57:48 -0300

    Aula 216

---
+++ src/Array/ArrayApp1.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class ArrayApp1 {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        array[0] = 2;
+        array[4] = 1;
+
+        System.out.println(array[0]);
+        System.out.println(array[4]);
+        System.out.println(array[5]);
+    }
+}
