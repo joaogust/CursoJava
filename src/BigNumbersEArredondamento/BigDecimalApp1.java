18a8eb82d8abbfe43e50359a3523e39535bf270d
joaogust <joaogustavodossantoss@gmail.com>
2025-03-11 14:57:31 -0300

    Aula 209

---
+++ src/BigNumbersEArredondamento/BigDecimalApp1.java
@@ -0,0 +1,16 @@
+package BigNumbersEArredondamento;
+
+import java.math.BigDecimal;
+
+public class BigDecimalApp1 {
+
+    public static void main(String[] args) {
+
+        BigDecimal big = new BigDecimal("123456789329.39");
+        big = big.multiply(BigDecimal.TEN);
+        big = big.add(BigDecimal.TEN);
+        big = big.divide(BigDecimal.TEN);
+
+        System.out.println(big);
+    }
+}
