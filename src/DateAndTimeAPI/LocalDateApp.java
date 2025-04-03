9d03f2c498ecd9a01d9ea6985e342b0e2f2e4ecf
joaogust <joaogustavodossantoss@gmail.com>
2025-04-02 23:31:17 -0300

    Aula 262

---
+++ src/DateAndTimeAPI/LocalDateApp.java
@@ -0,0 +1,19 @@
+package DateAndTimeAPI;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateApp {
+
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.of(2050, 1, 1);
+        System.out.println(date);
+
+        LocalDate date2 = LocalDate.parse("2050-01-01");
+        System.out.println(date2);
+
+        LocalDate date3 = LocalDate.parse("01/01/2050", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        System.out.println(date3);
+    }
+}
