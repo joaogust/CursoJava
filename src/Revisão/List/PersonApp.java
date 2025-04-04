d220a89ad41387a76981b51666af422d853d2351
joaogust <joaogustavodossantoss@gmail.com>
2025-04-03 22:36:08 -0300

    Revisão Comparator e Comparable

---
+++ src/Revisão/List/PersonApp.java
@@ -0,0 +1,23 @@
+package Revisão.List;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PersonApp {
+
+    public static void main(String[] args) {
+
+        var list = new ArrayList<Person>();
+
+        list.add(new Person("João", 19));
+        list.add(new Person("Ana", 17));
+        list.add(new Person("Islaine", 52));
+
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+        Collections.sort(list, new PersonComparator());
+        System.out.println(list);
+    }
+}
