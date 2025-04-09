4d3bafa3beb423342d8ad242c990ecba92a88a1a
joaogust <joaogustavodossantoss@gmail.com>
2025-04-08 22:37:59 -0300

    Aula 292

---
+++ src/ArquivosEDiretorios/FileApp5.java
@@ -0,0 +1,21 @@
+package ArquivosEDiretorios;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class FileApp5 {
+
+    public static void main(String[] args) throws IOException {
+
+        Path from = Path.of("src", "ArquivosEDiretorios", "file.txt");
+//        Path to = from.getParent().resolve("file.txt");
+        Path to = from.getParent().resolve(Path.of("files", "file2.txt"));
+
+        Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
+        // StandardCopyOption.REPLACE_EXISTING >> substitui o arquivo de destino caso ele exista
+
+        Files.move(from, to, StandardCopyOption.REPLACE_EXISTING);
+    }
+}
